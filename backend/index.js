2b1f349d518532042d5967f297fa5f85cbca6fce
VyomBajaj <vyombajaj04@gmail.com>
2025-05-23 14:10:12 +0530

    Models

--- backend/index.js
+++ backend/index.js
@@ -1,12 +1,17 @@
 import dotenv from 'dotenv';
 import { connectToMongo } from './db.js';
 import express from 'express'
+import authRouter from './routes/auth.js';
+import notesRouter from './routes/notes.js'
 
 dotenv.config()
 connectToMongo();
 
 const app = express()
 const PORT = 8000
+
+app.use('/api/auth',authRouter)
+app.use('/api/notes',notesRouter)
 
 app.get('/',(req,res)=>{
     res.send("Hello World")
