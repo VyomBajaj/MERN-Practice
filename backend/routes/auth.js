cfeb2f5f4a1b13e527d7b871529fbbe6e47b9d8f
VyomBajaj <vyombajaj04@gmail.com>
2025-05-23 16:12:51 +0530

    Sign up jwt and bcrypt

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,6 +1,8 @@
 import express from 'express'
 import {body,validationResult} from 'express-validator'
 import { User } from '../models/user.model.js';
+import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken"
 
 const router = express.Router();
 
@@ -17,17 +19,30 @@ router.post('/',[
 
     const {name,email,password} = req.body;
 
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
+
     try {
         const existingUser = await User.findOne({ email });
         if (existingUser) {
             return res.status(400).json({ error: 'Email already exists' });
         }
 
-        const user = new User({ name, email, password });
+        const user = new User({ name, email, password:hashedPassword });
         await user.save();
 
+        const data = {
+            user:{
+                "id":user._id
+            }
+        }
+
+        const authToken = jwt.sign(data,process.env.JWT_KEY)
+
+        // console.log(data);
+
         console.log("User Created Successfully")
-        res.status(201).json({ message: 'User registered successfully', user });
+        res.status(201).json({ message: 'User registered successfully', authToken });
 
     } catch (err) {
         console.error(err);
