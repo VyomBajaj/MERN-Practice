5fc1cf6e2df7a3163570cd6fbbd973ad0ebeda17
VyomBajaj <vyombajaj04@gmail.com>
2025-05-24 15:10:39 +0530

    Update and Delete Route

--- backend/routes/notes.js
+++ backend/routes/notes.js
@@ -1,34 +1,80 @@
 import express from 'express'
 import { fetchUser } from '../middleware/fetchUser.js';
 import { Notes } from '../models/notes.model.js';
-import {body,validationResult} from 'express-validator'
+import { body, validationResult } from 'express-validator'
 
 const router = express.Router();
 
 //Route 1 - Fetch All Notes (login Required)
-router.get('/fetchallnotes',fetchUser,async(req,res)=>{
-    const notes = await Notes.find({user:req.user.id});
+router.get('/fetchallnotes', fetchUser, async (req, res) => {
+    const notes = await Notes.find({ user: req.user.id });
     res.json(notes)
 })
-
-router.post('/addnote',fetchUser,[
-    body('title','Title should not be empty').isLength({min:1}),
-    body('description','Enter a valid description minLength - 5').isLength({min:5})
-],async(req,res)=>{
+// Route 2 - Add Notes (login Required)
+router.post('/addnote', fetchUser, [
+    body('title', 'Title should not be empty').isLength({ min: 1 }),
+    body('description', 'Enter a valid description minLength - 5').isLength({ min: 5 })
+], async (req, res) => {
     const errors = validationResult(req);
-    if(!errors.isEmpty()){
-        return res.status(400).json({errors:errors.array()});
+    if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() });
     }
-    const {title,description,tag} = req.body;
+    const { title, description, tags } = req.body;
 
     const note = new Notes({
         title,
         description,
-        tag,
-        user:req.user.id
+        tags,
+        user: req.user.id
     })
     const savedNote = await note.save();
     res.json(savedNote)
+})
+
+//Route 3 - Update an existing note
+router.put('/updatenote/:id', fetchUser, async (req, res) => {
+    try {
+        let note = await Notes.findById(req.params.id);
+        if (!note) {
+            return res.status(404).send("Not Found")
+        }
+        const userId = req.user.id
+        if (note.user.toString() != userId) {
+            return res.status(401).send("You are not allowed to update this note")
+        }
+        const { title, description, tags } = req.body;
+        const newNote = {};
+        if (title) {
+            newNote.title = title
+        }
+        if (description) { newNote.description = description; }
+        if (tags) { newNote.tags = tags; }
+
+        note = await Notes.findByIdAndUpdate(req.params.id, { $set: newNote }, { new: true })
+        res.json({ note })
+    } catch (error) {
+        res.json({error:error.message})
+    }
+
+})
+//Route 4 - Delete a particular note (Login Required)
+router.delete('/deletenote/:id', fetchUser, async (req, res) => {
+    try {
+        let note = await Notes.findById(req.params.id);
+        if (!note) {
+            return res.status(404).send("Not Found")
+        }
+        const userId = req.user.id
+        if (note.user.toString() != userId) {
+            return res.status(401).send("You are not allowed to delete this note")
+        }
+        note = await Notes.findByIdAndDelete(req.params.id)
+        res.json({"Success":"Note has been deleted successfully","note":note})
+
+    } catch (error) {
+        res.json({error:error.message})
+    }
+
 })
 
 export default router
